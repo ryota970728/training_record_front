bc906ffb4acc04e2fdad7853e68ebd0e6b95428d
RyotaHira <rhndpft2807@gmail.com>
2025-03-17 23:04:54 +0900

    vue.config.js修正。

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,6 @@ module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/training_record_front/' : '/',
   outputDir: 'docs'
 }
