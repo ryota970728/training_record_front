9e2623e34684e32f38e5441d0b5bf2881d5c7163
RyotaHira <rhndpft2807@gmail.com>
2025-03-18 22:08:10 +0900

    Constant本番・開発環境URL切り替え

--- src/Constant.js
+++ src/Constant.js
@@ -1,11 +1,7 @@
 export const FUNCTIONS_URL = {
-  GET_PART: "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/fetchPart",
-  GET_MENU: "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/fetchMenu",
-  POST_RECORD: "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/insertRecord",
-  GET_RECORDS: "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/fetchRecords",
-  // GET_PART: "http://127.0.0.1:54321/functions/v1/training_record/fetchPart",
-  // GET_MENU: "http://127.0.0.1:54321/functions/v1/training_record/fetchMenu",
-  // POST_RECORD: "http://127.0.0.1:54321/functions/v1/training_record/insertRecord",
-  // GET_RECORDS: "http://127.0.0.1:54321/functions/v1/training_record/fetchRecords",
+  GET_PART: process.env.NODE_ENV === 'production' ? "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/fetchPart" : "http://127.0.0.1:54321/functions/v1/training_record/fetchPart",
+  GET_MENU: process.env.NODE_ENV === 'production' ? "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/fetchMenu" : "http://127.0.0.1:54321/functions/v1/training_record/fetchMenu",
+  POST_RECORD: process.env.NODE_ENV === 'production' ? "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/insertRecord" : "http://127.0.0.1:54321/functions/v1/training_record/insertRecord",
+  GET_RECORDS: process.env.NODE_ENV === 'production' ? "https://cnhgmlmnsmfvawxmcoai.supabase.co/functions/v1/training_record/fetchRecords" : "http://127.0.0.1:54321/functions/v1/training_record/fetchRecords",
   AUTHORIZATION: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
 }
